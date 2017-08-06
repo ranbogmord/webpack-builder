796f291bc76d493d23b4a037f9dbe3131a6f8581
John Eriksson <john@provideit.se>
2017-08-06 11:27:24 +0200

    Added tests and some cleaning up

--- test/builder.spec.js
+++ test/builder.spec.js
@@ -43,6 +43,64 @@ describe('Webpack Builder', () => {
 		expect(builder._sass).to.contain(resource);
 	});
 
-	it('should ')
+	it('should throw an error when calling "::js" without a resource', () => {
+		builder = new Builder();
+		expect(builder.js.bind(builder)).to.throw();
+	});
+
+	it('should throw an error when calling "::sass" without a resource', () => {
+		builder = new Builder();
+		expect(builder.sass.bind(builder)).to.throw();
+	});
+
+	it('should throw an error if building with no assets', () => {
+		builder = new Builder();
+		expect(builder.build.bind(builder)).to.throw();
+	});
+
+	it('should return an object when calling "::build"', () => {
+		builder = new Builder();
+		builder.js('temp.js');
+		expect(builder.build()).to.be.an('object');
+	});
+
+	it('should have two plugins loaded', () => {
+		builder = new Builder();
+		builder.js('temp.js');
+		let cfg = builder.build();
+		expect(cfg.plugins).to.be.an('array');
+		expect(cfg.plugins).to.have.lengthOf(2);
+	});
+
+	it('should have an object of entries after build', () => {
+		builder = new Builder();
+		builder.js('test.js');
+		let cfg = builder.build();
+
+		expect(cfg.entry).to.be.an('object');
+	});
+
+	it('should have a key named after the file in "entry"', () => {
+		builder = new Builder();
+		let name = "test";
+		builder.js(name + '.scss');
+		let cfg = builder.build();
+
+		expect(cfg.entry).to.have.all.keys('test');
+	});
+
+	it('should be able to handle multiple items with the same name', () => {
+		builder = new Builder();
+		let name = "test";
+		builder.js(name + '.js');
+		builder.sass(name + '.scss');
+
+		let cfg = builder.build();
+
+		expect(cfg.entry).to.have.all.keys(name);
+		expect(cfg.entry[name]).to.be.an('array');
+		expect(cfg.entry[name]).to.contain(name + '.js');
+		expect(cfg.entry[name]).to.contain(name + '.scss');
+	});
 });
 
